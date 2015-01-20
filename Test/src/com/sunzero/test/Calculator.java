eced16f1e9d87ee00f8b44e4ff8a888781f2ae15
SeoDaeryong <wotnek2@naver.com>
2015-01-20 17:47:11 +0900

    Test 코드 추가

---
+++ Test/src/com/sunzero/test/Calculator.java
@@ -0,0 +1,10 @@
+package com.sunzero.test;
+
+public class Calculator {
+
+	public int add(int a, int b) {
+		// TODO Auto-generated method stub
+		return a + b;
+	}
+
+}
---
+++ Test/test/com/sunzero/test/TestCalculator.java
@@ -0,0 +1,15 @@
+package com.sunzero.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCalculator {
+
+	private Calculator cal = new Calculator();
+	@Test
+	public void testAdd() {
+		assertEquals(cal.add(1,2), 3);
+	}
+
+}
